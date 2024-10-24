0b232246ba6f5ad66684e85a299708459ab19122
OsamaAbdulHafeez <140329663+OsamaAbdulHafeez@users.noreply.github.com>
2024-10-24 10:44:47 +0500

    react big calender on working

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -9,6 +9,7 @@ import React from 'react'
 const AdminPage = () => {
   return (
     <div className='p-4 flex flex-col md:flex-row gap-4'>
+      {/* LEFT */}
       <div className='w-full lg:w-2/3 flex flex-col gap-8'>
         <div className='flex gap-4 justify-between flex-wrap'>
           <UserCard type='student'/>
@@ -32,6 +33,7 @@ const AdminPage = () => {
           <FinanceChart/>
         </div>
       </div>
+      {/* RIGHT */}
       <div className='w-full lg:w-1/3 flex flex-col gap-8'>
         <EventCalender/>
         <Announcements/>
