e5a4368f0baaacc389a88fea0ac97a73c02eb4d1
OsamaAbdulHafeez <140329663+OsamaAbdulHafeez@users.noreply.github.com>
2024-10-24 17:47:32 +0500

    teacher,parent page done

---
+++ src/app/(dashboard)/list/teacher/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeacherListPage = () => {
+  return (
+    <div className='p-4 bg-white m-4 flex-1 rounded-md mt-0'>
+      Teacher List
+    </div>
+  )
+}
+
+export default TeacherListPage
--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,10 +1,23 @@
+import Announcements from '@/components/Announcements'
+import BigCalendar from '@/components/BigCalender'
+import EventCalender from '@/components/EventCalender'
 import React from 'react'
 
 const ParentPage = () => {
   return (
-    <div>
-      ParentPage
+    <div className='flex-1 p-4 flex flex-col xl:flex-row gap-4'>
+    {/* LEFT */}
+    <div className='w-full xl:w-2/3 flex flex-col gap-8'>
+      <div className='h-full bg-white rounded-md p-4'>
+        <h1 className='text-xl font-semibold'>Schedule (Osama)</h1>
+        <BigCalendar/>
+      </div>
     </div>
+    {/* RIGHT */}
+    <div className='w-full xl:w-1/3 flex flex-col gap-8'>
+      <Announcements/>
+    </div>
+  </div>
   )
 }
 
